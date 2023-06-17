063491a35230318a698aa7b9cab367325f0f2472
Guilherme Varela <guilhermevareladossantos@gmail.com>
2023-06-17 16:02:21 -0300

    Inicia página de Calendário

---
+++ src/pages/CalendarioPage/CalendarioPage.vue
@@ -0,0 +1,36 @@
+<template>
+  <HeaderComponent />
+  <div class="container">
+    <main>
+      <h1>Calendário dos Cursos ministrados por você</h1>
+
+    </main>
+  </div>
+</template>
+
+<script>
+import HeaderComponent from '@/components/HeaderComponent.vue'
+export default {
+  components: {
+    HeaderComponent
+  },
+}
+</script>
+
+<style scoped>
+  .container {
+    margin-top: 4rem;
+    width: 100%;
+    display: flex;
+    align-content: center;
+    justify-content: center;
+  }
+
+  main {
+    max-width: 1080px;
+    height: 400px;
+    padding: 1rem 2rem;
+    background-color: #fff;
+  }
+ 
+</style>
