3503bcfa1db7799faeb592d83fb1e206903e68b1
karenpacheco16 <karenpacheco16@poli.ufrj.br>
2023-06-18 22:53:44 -0300

    Implementando logica de visualizacao do botao de edicao. Está disponivel para o perfil de professor #9

--- src/pages/DetalhesDisciplinaPage/DisciplinaDetalhesPage.vue
+++ src/pages/DetalhesDisciplinaPage/DisciplinaDetalhesPage.vue
@@ -5,8 +5,13 @@
     <v-main>
       <div class="external">
         <div class="container">
-          <h2>{{ discipline.cod }} </h2>
-          <h2>{{ discipline.name }}</h2>
+          <div class="code-container">
+            <h2> {{ discipline.cod }} </h2>
+            <v-btn v-if="userType === 'professor'" class="edit-button" icon>
+              <v-icon>mdi-pencil</v-icon>
+            </v-btn>
+          </div>
+          <h2 class="custom-name-title">{{ discipline.name }}</h2>
           <h3>Cursos ofertados</h3>
           <ul>
             <li v-for="course in discipline.courses" :key="course.id">{{ course.name }}</li>
@@ -14,7 +19,8 @@
           <h3>Provas</h3>
           <ul>
             <li v-for="examDate in discipline.examDates" :key="examDate.id">
-              <span class="custom-description-exam">{{ examDate.description }} </span> <span>{{ examDate.date }}</span>
+              <span class="custom-description-exam">{{ examDate.description }}</span>
+              <span>{{ examDate.date }}</span>
             </li>
           </ul>
         </div>
@@ -22,7 +28,7 @@
     </v-main>
   </v-app>
 </template>
-  
+
 <script>
 import HeaderComponent from '../../components/HeaderComponent.vue';
 
@@ -32,6 +38,7 @@ export default {
   },
   data() {
     return {
+      userType: 'professor', // Logica de visualizacao do botao de edicao. Só está disponivel para o perfil de professores
       discipline: {
         cod: "COS123",
         name: "Inteligencia Artificial",
@@ -40,13 +47,13 @@ export default {
           { id: 2, name: "Engenharia de Computação e Informação" },
         ],
         examDates: [
-          { id: 1, description:"P1", date: "22/05/2023" },
-          { id: 2, description:"P2",date: "20/07/2023" },
+          { id: 1, description: "P1", date: "22/05/2023" },
+          { id: 2, description: "P2", date: "20/07/2023" },
         ],
       },
     };
   },
 };
 </script>
-  
+
 <style src="./style.css" scoped></style>
