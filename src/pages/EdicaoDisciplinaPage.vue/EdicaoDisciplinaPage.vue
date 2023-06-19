e9911c95b3b061ff5337aec6c0959d4a3e501d19
karenpacheco16 <karenpacheco16@poli.ufrj.br>
2023-06-19 01:57:33 -0300

    Implementando logica dos botoes de edicao e exclusao dos campos #3

--- src/pages/EdicaoDisciplinaPage.vue/EdicaoDisciplinaPage.vue
+++ src/pages/EdicaoDisciplinaPage.vue/EdicaoDisciplinaPage.vue
@@ -19,17 +19,33 @@
               <v-icon>mdi-pencil</v-icon>
             </v-btn>
           </div>
-          
-          
+
           <h3>Cursos ofertados</h3>
           <ul>
-            <li v-for="course in discipline.courses" :key="course.id">{{ course.name }}</li>
+            <li v-for="course in discipline.courses" :key="course.id">
+              <span>{{ course.name }}</span>
+              <v-icon v-if="userType === 'professor'" class="delete-icon" @click="deleteCourse(course.id)">
+                mdi-trash-can-outline
+              </v-icon>
+            </li>
           </ul>
+
+          <div class="code-container">
+            <v-autocomplete v-model="newCourse" :items="availableCourses" item-title="name" item-value="id"
+              label="Buscar curso" clearable></v-autocomplete>
+            <v-btn v-if="userType === 'professor'" class="edit-button" icon @click="addCourse">
+              <v-icon>mdi-plus</v-icon>
+            </v-btn>
+          </div>
+
           <h3>Provas</h3>
           <ul>
             <li v-for="examDate in discipline.examDates" :key="examDate.id">
               <span class="custom-description-exam">{{ examDate.description }}</span>
               <span>{{ examDate.date }}</span>
+              <v-icon v-if="userType === 'professor'" class="delete-icon" @click="deleteExamDate(examDate.id)">
+                mdi-trash-can-outline
+              </v-icon>
             </li>
           </ul>
         </div>
@@ -62,7 +78,19 @@ export default {
           { id: 2, description: "P2", date: "20/07/2023" },
         ],
       },
+      filteredCourses: [
+        { id: 3, name: "Engenharia Mecânica" },
+        { id: 4, name: "Engenharia Elétrica" },
+        { id: 5, name: "Ciência da Computação" },
+        { id: 6, name: "Engenharia Eletrônica e de Computação" },
+      ],
+      newCourse: null,
     };
+  },
+  computed: {
+    availableCourses() {
+      return this.filteredCourses.filter(course => !this.discipline.courses.some(c => c.id === course.id));
+    },
   },
   methods: {
     editCodDiscipline() {
@@ -70,6 +98,21 @@ export default {
     },
     editNameDiscipline() {
       this.isEditingName = !this.isEditingName;
+    },
+    addCourse() {
+      if (this.newCourse) {
+        const selectedCourse = this.filteredCourses.find(course => course.id === this.newCourse);
+        if (selectedCourse) {
+          this.discipline.courses.push(selectedCourse);
+          this.newCourse = null;
+        }
+      }
+    },
+    deleteCourse(courseId) {
+      this.discipline.courses = this.discipline.courses.filter(course => course.id !== courseId);
+    },
+    deleteExamDate(examDateId) {
+      this.discipline.examDates = this.discipline.examDates.filter(examDate => examDate.id !== examDateId);
     },
   },
 };
